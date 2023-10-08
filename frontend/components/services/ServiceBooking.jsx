3d2a0513c60e6f488f04484dc54dd8c59f4544a2
Dinushkasam <dinushkasam@gmail.com>
2023-10-08 21:54:44 +0530

    hire, get, check service

--- frontend/components/services/ServiceBooking.jsx
+++ frontend/components/services/ServiceBooking.jsx
@@ -18,16 +18,22 @@ import Animated, {
 } from "react-native-reanimated";
 import DailyBooking from "./DailyBooking";
 import WeeklyBooking from "./WeeklyBooking";
+import axios from "axios";
 
 const BOOKING_TYPES = ["ONE_TIME", "DAILY", "WEEKLY"];
+//TODO: remove temp user
+const USER = {
+    _id: "6519178b1a22eacde138ed61",
+};
 
 const ServiceBooking = ({ navigation, route }) => {
     const { theme } = getThemeContext();
-    const { tabColor } = getAppContext();
     const { service } = route.params;
+    const { SERVER_URL } = getAppContext();
     const [bookingType, setBookingType] = useState(BOOKING_TYPES[0]);
     const [prevType, setPrevType] = useState(BOOKING_TYPES[0]); //for animation [0,1,2]
     const [allDay, setAllDay] = useState(false);
+    const [continuous, setContinuous] = useState(false); //for daily booking [0,1
     const [oneDay, setOneDay] = useState(false);
     const [datePicker, setDatePicker] = useState({
         show: false,
@@ -38,12 +44,10 @@ const ServiceBooking = ({ navigation, route }) => {
 
     //input data
     const [input, setInput] = useState({
-        startDate: new Date(Date.now()),
-        endDate: new Date(Date.now()),
-        startTime: new Date(Date.now()),
-        endTime: new Date(Date.now()),
+        startDateTime: new Date(Date.now()),
+        endDateTime: new Date(Date.now()),
         pets: [],
-        paymentMethod: null,
+        paymentMethod: "credit",
         notes: "",
         days: new Array(7).fill(false),
     });
@@ -90,6 +94,47 @@ const ServiceBooking = ({ navigation, route }) => {
         },
     ];
 
+    const handleHirePress = async () => {
+        //calculate fees
+        const fee = service.services.fees.find(
+            (fee) => fee.tag === bookingType
+        ).price;
+        const totalFee = calculateFees(
+            input,
+            bookingType,
+            allDay,
+            fee,
+            continuous,
+            oneDay
+        );
+
+        const reqData = {
+            user: USER._id,
+            serviceProvider: service._id,
+            involvedPets: [], //TODO: input.pets.map((pet) => pet._id),
+            startDate: input.startDateTime.toLocaleDateString(),
+            endDate: oneDay
+                ? "continuous"
+                : input.endDateTime.toLocaleDateString(),
+            startTime: allDay
+                ? "12:00:00 AM"
+                : input.startDateTime.toLocaleTimeString(),
+            endTime: allDay
+                ? "11:59:59 PM"
+                : input.endDateTime.toLocaleTimeString(),
+            daily: bookingType === BOOKING_TYPES[1],
+            weekly: bookingType === BOOKING_TYPES[2],
+            days: input.days,
+            oneDay: oneDay,
+            continuous: continuous,
+            totalFee: totalFee,
+            notes: input.notes,
+            paymentMethod: input.paymentMethod,
+        };
+        
+        //const response = await axios.post(`${SERVER_URL}/api/v1/hire`, reqData);
+    };
+
     return (
         <View
             style={{
@@ -124,6 +169,8 @@ const ServiceBooking = ({ navigation, route }) => {
                             setDatePicker={setDatePicker}
                             input={input}
                             setInput={setInput}
+                            oneDay={oneDay}
+                            setOneDay={setOneDay}
                             allDay={allDay}
                             setAllDay={setAllDay}
                             theme={theme}
@@ -133,15 +180,23 @@ const ServiceBooking = ({ navigation, route }) => {
 
                 {bookingType === BOOKING_TYPES[1] && (
                     <Animated.View
-                        entering={prevType===BOOKING_TYPES[2] ? SlideInLeft : SlideInRight}
-                        exiting={prevType===BOOKING_TYPES[0] ? SlideOutLeft : SlideOutRight}>
+                        entering={
+                            prevType === BOOKING_TYPES[2]
+                                ? SlideInLeft
+                                : SlideInRight
+                        }
+                        exiting={
+                            prevType === BOOKING_TYPES[0]
+                                ? SlideOutLeft
+                                : SlideOutRight
+                        }>
                         <DailyBooking
                             styles={styles}
                             setDatePicker={setDatePicker}
                             input={input}
                             setInput={setInput}
-                            oneDay={oneDay}
-                            setOneDay={setOneDay}
+                            oneDay={continuous}
+                            setOneDay={setContinuous}
                             allDay={allDay}
                             setAllDay={setAllDay}
                             theme={theme}
@@ -169,8 +224,11 @@ const ServiceBooking = ({ navigation, route }) => {
             <Animated.View
                 entering={FadeInDown}
                 exiting={FadeOutDown}
-                style={{marginBottom:10}}>
-                <ThemeButton title={"Hire"} textSize={16}>
+                style={{ marginBottom: 10 }}>
+                <ThemeButton
+                    title={"Hire"}
+                    textSize={16}
+                    onPress={handleHirePress}>
                     <Ionicons
                         name="add-circle-outline"
                         size={24}
@@ -195,3 +253,62 @@ const ServiceBooking = ({ navigation, route }) => {
 };
 
 export default ServiceBooking;
+
+const calculateFees = (input, bookingType, allDay, fee, continuous, oneDay) => {
+    let totalFee;
+    let oneDayTime = new Date(
+        input.startDateTime.toISOString().split("T")[0] +
+            "T" +
+            input.endDateTime.toISOString().split("T")[1]
+    );
+
+    let sameDayTimeDifference = (oneDayTime - input.startDateTime) / 3600000;
+    let timeRangeDateDifference =
+        Math.floor(
+            (new Date(input.endDateTime.toISOString().split("T")[0]) -
+                new Date(input.startDateTime.toISOString().split("T")[0])) /
+                86400000
+        ) + 1;
+
+    if (bookingType === BOOKING_TYPES[2]) {
+        let numDays = input.days.filter((day) => day).length;
+        if (allDay) {
+            totalFee = numDays * 24 * fee;
+        } else {
+            totalFee =
+                numDays * ((oneDayTime - input.startDateTime) / 3600000) * fee;
+        }
+    } else if (bookingType === BOOKING_TYPES[1]) {
+        if (allDay) {
+            if (continuous) {
+                totalFee = fee * 24;
+            } else {
+                totalFee = timeRangeDateDifference * 24 * fee;
+            }
+        } else {
+            if (continuous) {
+                totalFee = fee * sameDayTimeDifference;
+            } else {
+                totalFee =
+                    timeRangeDateDifference * sameDayTimeDifference * fee;
+            }
+        }
+    } else {
+        if (allDay) {
+            if (oneDay) {
+                totalFee = fee * 24;
+            } else {
+                totalFee = fee * 24 * timeRangeDateDifference;
+            }
+        } else {
+            if (oneDay) {
+                totalFee = fee * sameDayTimeDifference;
+            } else {
+                totalFee =
+                    fee * sameDayTimeDifference * timeRangeDateDifference;
+            }
+        }
+    }
+
+    return totalFee;
+};
--- frontend/components/services/WeeklyBooking.jsx
+++ frontend/components/services/WeeklyBooking.jsx
@@ -69,15 +69,15 @@ const WeeklyBooking = ({
                             show: true,
                             mode: "time",
                             inputCallback: (date) => {
-                                setInput({ ...input, startTime: date });
+                                setInput({ ...input, startDateTime: date });
                             },
-                            date: input.startTime,
+                            date: input.startDateTime,
                         });
                     }}
                     value={
                         allDay
                             ? "12:00:00 AM"
-                            : input.startTime.toLocaleTimeString()
+                            : input.startDateTime.toLocaleTimeString()
                     }
                     width={"45%"}
                     editable={false}
@@ -101,15 +101,15 @@ const WeeklyBooking = ({
                             show: true,
                             mode: "time",
                             inputCallback: (date) => {
-                                setInput({ ...input, endTime: date });
+                                setInput({ ...input, endDateTime: date });
                             },
-                            date: input.endTime,
+                            date: input.endDateTime,
                         });
                     }}
                     value={
                         allDay
                             ? "11:59:59 PM"
-                            : input.endTime.toLocaleTimeString()
+                            : input.endDateTime.toLocaleTimeString()
                     }
                     icon={
                         <FontAwesome5
