e57e8213d739c7f9e0511dccb0b7e86b6cf8a2f9
Dinushkasam <dinushkasam@gmail.com>
2023-10-09 20:12:56 +0530

    service controller hint index

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -144,7 +144,7 @@ const getHireRequests = asyncHandler(async (req, res) => {
                     },
                 },
             }
-        ]);
+        ]).hint({ user: 1, serviceProvider: 1 });
         res.json(hireRequests);
     } catch (error) {
         res.json({error:error.message});
@@ -187,7 +187,7 @@ const getMyHireRequests = asyncHandler(async (req, res) => {
                     },
                 },
             }
-        ]);
+        ]).hint({ user: 1, serviceProvider: 1 })
         res.json(hireRequests);
     } catch (error) {
         res.json({error:error.message});
@@ -204,8 +204,10 @@ const checkHireRequests = asyncHandler(async (req, res) => {
             {
                 $match: {
                     serviceProvider: req.params.id,
-                    startDate: { $gte: startDate },
-                    endDate: { $lte: endDate },
+                    startDate: { $gte: new Date(startDate) },
+                    endDate: { $lte: new Date(endDate) },
+                    startTime: { $gte: new Date(startDate + "T" + startTime) },
+                    endTime: { $lte: new Date(startDate + "T" + endTime) },
                 }
             },{
                 $project: {
