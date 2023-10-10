6db9ace5fb014feb5f7b7b6b72733dea983075d9
Dinushkasam <dinushkasam@gmail.com>
2023-10-10 21:52:25 +0530

    rating routes fixed

--- backend/routes/ratingRoutes.js
+++ backend/routes/ratingRoutes.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import { createRating, getRatingById, getRatingByServiceProvider, getRatingByUserAndServiceProvider, updateRatingByUserAndServiceProvider } from '../controllers/ratingController';
+import {
+    createRating,
+    getRatingById,
+    getRatingByServiceProvider,
+    getRatingByUserAndServiceProvider,
+    updateRatingByUserAndServiceProvider,
+} from "../controllers/ratingController.js";
 
 const router = express.Router();
 
-router.get('/')
+router.route('/')
     .get(getRatingById)
     .post(createRating);
 
-router.get('/:id').get(getRatingByServiceProvider)
+router.route('/:id').get(getRatingByServiceProvider)
 
 // POST a new rating
-router.post('/user/:uid/:sid')
+router.route('/user/:uid/:sid')
     .get(getRatingByUserAndServiceProvider)
     .put(updateRatingByUserAndServiceProvider);
 
-module.exports = router;
+export default router;
