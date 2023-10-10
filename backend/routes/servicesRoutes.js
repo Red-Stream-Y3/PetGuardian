96918ed3735d266a30d812934c3d9ddf5bc1be4e
Dinushkasam <dinushkasam@gmail.com>
2023-10-10 19:40:38 +0530

    booking update and getbyid routes

--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import {
     checkHireRequests,
+    getHireRequestById,
     getHireRequests,
     getMyHireRequests,
     getProviderById,
     getProviders,
     hireProvider,
+    updateHireRequest,
 } from "../controllers/servicesController.js";
 
 const router = express.Router();
@@ -13,14 +15,17 @@ const router = express.Router();
 router.route('/').get(getProviders);
 router.route('/:id').get(getProviderById);
 
-router.route('/hire').post(hireProvider);
+router.route('/hire')
+    .post(hireProvider)
+    .put(updateHireRequest);
 
 router.route('/hire/:id')
-    .get(getHireRequests)
-    //.put(hireProvider);
+    .get(getHireRequests);
 
 router.route('/myhire/:id')
     .get(getMyHireRequests);
+
+router.route('/hire/getbyid/:id').get(getHireRequestById);
 
 router.route('/hire/check').post(checkHireRequests);
 
