fff6291c8d0c01c77576c52850e2e6165a6309c2
Dinushkasam <dinushkasam@gmail.com>
2023-10-19 17:28:18 +0530

    service provider search frontend

--- frontend/services/ServiceproviderSerives.js
+++ frontend/services/ServiceproviderSerives.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE = 'http://192.168.1.175:9120'; //"https://pet-shop-backend-ukkxew3r5q-uc.a.run.app";
+const BASE = 'https://pet-shop-backend-ukkxew3r5q-uc.a.run.app';
 
 export const getServiceProviders = async (token) => {
     const config = {
