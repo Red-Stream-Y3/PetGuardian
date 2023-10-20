655128fb3fb563361f7d381b5f2e5239af4e921d
Dinushkasam <dinushkasam@gmail.com>
2023-10-20 10:48:41 +0530

    commented shared transition

--- frontend/components/common/BottomBar.jsx
+++ frontend/components/common/BottomBar.jsx
@@ -4,158 +4,141 @@ import RoundIconButton from './RoundIconButton';
 import getThemeContext from '../../context/ThemeContext';
 import { Ionicons } from '@expo/vector-icons';
 import {
-    useNavigation,
-    useRoute,
-    NavigationState,
+  useNavigation,
+  useRoute,
+  NavigationState,
 } from '@react-navigation/native';
 import { getAppContext } from '../../context/AppContext';
 
 const BottomBar = ({}) => {
-    const { theme } = getThemeContext();
-    const { selectedTab, setSelectedTab, tabColor } = getAppContext();
-    const navigation = useNavigation();
-    const [barColor, setBarColor] = useState(theme.colors.homePrimary);
-    const [animation] = useState(new Animated.Value(0));
+  const { theme } = getThemeContext();
+  const { selectedTab, setSelectedTab, tabColor } = getAppContext();
+  const navigation = useNavigation();
+  const [barColor, setBarColor] = useState(theme.colors.homePrimary);
+  const [animation] = useState(new Animated.Value(0));
 
-    const getActiveRouteState = (route) => {
-        if (
-            !route.routes ||
-            route.routes.length === 0 ||
-            route.index >= route.routes.length
-        ) {
-            return route;
+  const getActiveRouteState = (route) => {
+    if (
+      !route.routes ||
+      route.routes.length === 0 ||
+      route.index >= route.routes.length
+    ) {
+      return route;
+    }
+
+    const childActiveRoute = route.routes[route.index];
+    return getActiveRouteState(childActiveRoute);
+  };
+
+  //on back press in android, set selected tab to home
+  useEffect(() => {
+    const backhandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (selectedTab !== 2) {
+          setSelectedTab(2);
+          return true;
         }
-
-        const childActiveRoute = route.routes[route.index];
-        return getActiveRouteState(childActiveRoute);
-    };
-
-    //on back press in android, set selected tab to home
-    useEffect(() => {
-        const backhandler = BackHandler.addEventListener(
-            'hardwareBackPress',
-            () => {
-                if (selectedTab !== 2) {
-                    setSelectedTab(2);
-                    return true;
-                }
-                return false;
-            }
-        );
-        return () => backhandler.remove();
-    }, [selectedTab]);
-
-    const PADDING = 8;
-    const SIZE_1 = 25;
-    const SIZE_2 = 30;
-    const ICON_THEME = theme.colors.primaryIcon;
-
-    const triggerAnimation = () => {
-        Animated.timing(animation, {
-            toValue: 1,
-            duration: 300,
-            useNativeDriver: false,
-        }).start(() => {
-            setBarColor(tabColor);
-            animation.setValue(0);
-        });
-    };
-
-    useEffect(() => {
-        navigation.navigate(buttons[selectedTab].name);
-    }, [selectedTab]);
-
-    useEffect(() => {
-        triggerAnimation();
-    }, [theme, tabColor]);
-
-    const handlePress = (index) => {
-        setSelectedTab(index);
-    };
-
-    const buttons = [
-        {
-            name: 'SERVICES',
-            icon: (
-                <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />
-            ),
-            selectIcon: (
-                <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />
-            ),
-            padding: PADDING,
-        },
-        {
-            name: 'LOST',
-            icon: (
-                <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />
-            ),
-            selectIcon: (
-                <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />
-            ),
-            padding: PADDING,
-        },
-        {
-            name: 'HOME',
-            icon: (
-                <Ionicons name="paw-outline" size={SIZE_2} color={ICON_THEME} />
-            ),
-            selectIcon: (
-                <Ionicons name="paw" size={SIZE_2} color={ICON_THEME} />
-            ),
-            padding: PADDING,
-        },
-        {
-            name: 'ADOPT',
-            icon: (
-                <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />
-            ),
-            selectIcon: (
-                <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />
-            ),
-            padding: PADDING,
-        },
-        {
-            name: 'PLAY',
-            icon: (
-                <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />
-            ),
-            selectIcon: (
-                <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />
-            ),
-            padding: PADDING,
-        },
-    ];
-
-    let backgroundColor = animation.interpolate({
-        inputRange: [0, 1],
-        outputRange: [barColor, tabColor],
-    });
-
-    const styles = StyleSheet.create({
-        container: {
-            alignItems: 'center',
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            backgroundColor: backgroundColor,
-            borderTopEndRadius: 10,
-            borderTopStartRadius: 10,
-        },
-    });
-
-    return (
-        <Animated.View style={styles.container}>
-            {buttons.map((button, index) => (
-                <RoundIconButton
-                    key={index}
-                    name={button.name}
-                    padding={button.padding}
-                    onPress={() => handlePress(index)}
-                >
-                    {selectedTab === index ? button.selectIcon : button.icon}
-                </RoundIconButton>
-            ))}
-        </Animated.View>
+        return false;
+      }
     );
+    return () => backhandler.remove();
+  }, [selectedTab]);
+
+  const PADDING = 8;
+  const SIZE_1 = 25;
+  const SIZE_2 = 30;
+  const ICON_THEME = theme.colors.primaryIcon;
+
+  const triggerAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      setBarColor(tabColor);
+      animation.setValue(0);
+    });
+  };
+
+  useEffect(() => {
+    navigation.navigate(buttons[selectedTab].name);
+  }, [selectedTab]);
+
+  useEffect(() => {
+    triggerAnimation();
+  }, [theme, tabColor]);
+
+  const handlePress = (index) => {
+    setSelectedTab(index);
+    setTimeout(() => {
+      //do nothing until the animation is done
+    }, 300);
+  };
+
+  const buttons = [
+    {
+      name: 'SERVICES',
+      icon: <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />,
+      selectIcon: <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />,
+      padding: PADDING,
+    },
+    {
+      name: 'LOST',
+      icon: <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />,
+      selectIcon: <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />,
+      padding: PADDING,
+    },
+    {
+      name: 'HOME',
+      icon: <Ionicons name="paw-outline" size={SIZE_2} color={ICON_THEME} />,
+      selectIcon: <Ionicons name="paw" size={SIZE_2} color={ICON_THEME} />,
+      padding: PADDING,
+    },
+    {
+      name: 'ADOPT',
+      icon: <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />,
+      selectIcon: <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />,
+      padding: PADDING,
+    },
+    {
+      name: 'PLAY',
+      icon: <Ionicons name="paw-outline" size={SIZE_1} color={ICON_THEME} />,
+      selectIcon: <Ionicons name="paw" size={SIZE_1} color={ICON_THEME} />,
+      padding: PADDING,
+    },
+  ];
+
+  let backgroundColor = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [barColor, tabColor],
+  });
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      backgroundColor: backgroundColor,
+      borderTopEndRadius: 10,
+      borderTopStartRadius: 10,
+    },
+  });
+
+  return (
+    <Animated.View style={styles.container}>
+      {buttons.map((button, index) => (
+        <RoundIconButton
+          key={index}
+          name={button.name}
+          padding={button.padding}
+          onPress={() => handlePress(index)}
+        >
+          {selectedTab === index ? button.selectIcon : button.icon}
+        </RoundIconButton>
+      ))}
+    </Animated.View>
+  );
 };
 
 export default BottomBar;
--- frontend/components/common/ImageItemCard.jsx
+++ frontend/components/common/ImageItemCard.jsx
@@ -1,159 +1,151 @@
 import { Suspense, useEffect } from 'react';
 import {
-    ActivityIndicator,
-    Pressable,
-    StyleSheet,
-    Text,
-    View,
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
 import getThemeContext from '../../context/ThemeContext';
 import Animated, { FadeInDown } from 'react-native-reanimated';
 
 const ImageItemCard = ({
-    image,
-    uri,
-    onClick,
-    body,
-    title,
-    sideTag,
-    subtitle,
-    style,
-    width,
-    borderRadius,
-    viewMarginBottom,
-    textMarginBottom,
-    animationTag,
-    index,
-    highlight,
-    highlightColor,
+  image,
+  uri,
+  onClick,
+  body,
+  title,
+  sideTag,
+  subtitle,
+  style,
+  width,
+  borderRadius,
+  viewMarginBottom,
+  textMarginBottom,
+  animationTag,
+  index,
+  highlight,
+  highlightColor,
 }) => {
-    const { theme } = getThemeContext();
-    const imageStyles = {
-        fill: {
-            width: '100%',
-            height: 150,
-            resizeMode: 'cover',
-            borderRadius: borderRadius || 0,
-        },
-        side: {
-            width: 150,
-            height: '100%',
-            resizeMode: 'cover',
-            borderRadius: borderRadius || 0,
-        },
-    };
+  const { theme } = getThemeContext();
+  const imageStyles = {
+    fill: {
+      width: '100%',
+      height: 150,
+      resizeMode: 'cover',
+      borderRadius: borderRadius || 0,
+    },
+    side: {
+      width: 150,
+      height: '100%',
+      resizeMode: 'cover',
+      borderRadius: borderRadius || 0,
+    },
+  };
 
-    useEffect(() => {
-        if (style === 'side') {
-            if (!body) {
-                console.warn(
-                    'ImageItemCard: style=side requires a body element to be provided'
-                );
+  useEffect(() => {
+    if (style === 'side') {
+      if (!body) {
+        console.warn(
+          'ImageItemCard: style=side requires a body element to be provided'
+        );
+      }
+      //   if (width) {
+      //     console.warn('ImageItemCard: style=side does not support width prop');
+      //   }
+    }
+  }, []);
+
+  const styles = StyleSheet.create({
+    container: {
+      width: width || '90%',
+      maxWidth: width || 500,
+      margin: 10,
+      shadowColor: theme.colors.shadow,
+      elevation: 3,
+      backgroundColor: theme.colors.surface,
+      borderRadius: borderRadius || 10,
+      overflow: 'hidden',
+    },
+    ripple: {
+      color: theme.colors.ripple,
+    },
+    pressableContainer: {
+      height: style === 'side' ? 150 : 'auto',
+      flexDirection: style === 'side' ? 'row' : 'column',
+    },
+    bodyContainer: {
+      flex: 1,
+      padding: 10,
+      marginBottom: viewMarginBottom || 0,
+      flexDirection: style === 'side' ? 'column' : 'row',
+      justifyContent: style === 'side' ? 'center' : 'space-between',
+      borderStartWidth: style === 'side' && highlight ? 5 : 0,
+      borderStartColor:
+        style === 'side' && highlight ? highlightColor : theme.colors.surface,
+    },
+    titleText: {
+      fontWeight: 'bold',
+      color: theme.colors.text,
+      marginBottom: textMarginBottom || 0,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    subtitleText: {
+      color: theme.colors.text,
+      fontSize: 12,
+      fontWeight: '500',
+    },
+    tagText: {
+      color: theme.colors.text,
+      position: 'absolute',
+      fontSize: 12,
+      fontWeight: 'bold',
+      top: 11.2,
+      right: 10,
+    },
+  });
+
+  return (
+    <Animated.View
+      style={styles.container}
+      entering={FadeInDown.delay(index ? index * 100 : 0)}
+    >
+      <Pressable
+        android_ripple={styles.ripple}
+        style={styles.pressableContainer}
+        onPress={onClick || null}
+      >
+        <Suspense fallback={<ActivityIndicator />}>
+          <Animated.Image
+            style={style === 'side' ? imageStyles.side : imageStyles.fill}
+            source={
+              image || {
+                uri:
+                  uri ||
+                  'https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png',
+              }
             }
-            //   if (width) {
-            //     console.warn('ImageItemCard: style=side does not support width prop');
-            //   }
-        }
-    }, []);
+            //sharedTransitionTag={animationTag || null}
+          />
+        </Suspense>
+        <View style={styles.bodyContainer}>
+          <View>
+            {title ? <Text style={styles.titleText}>{title}</Text> : null}
+            {subtitle ? (
+              <Text style={styles.subtitleText}>{subtitle}</Text>
+            ) : null}
+          </View>
 
-    const styles = StyleSheet.create({
-        container: {
-            width: width || '90%',
-            maxWidth: width || 500,
-            margin: 10,
-            shadowColor: theme.colors.shadow,
-            elevation: 3,
-            backgroundColor: theme.colors.surface,
-            borderRadius: borderRadius || 10,
-            overflow: 'hidden',
-        },
-        ripple: {
-            color: theme.colors.ripple,
-        },
-        pressableContainer: {
-            height: style === 'side' ? 150 : 'auto',
-            flexDirection: style === 'side' ? 'row' : 'column',
-        },
-        bodyContainer: {
-            flex: 1,
-            padding: 10,
-            marginBottom: viewMarginBottom || 0,
-            flexDirection: style === 'side' ? 'column' : 'row',
-            justifyContent: style === 'side' ? 'center' : 'space-between',
-            borderStartWidth: style === 'side' && highlight ? 5 : 0,
-            borderStartColor:
-                style === 'side' && highlight
-                    ? highlightColor
-                    : theme.colors.surface,
-        },
-        titleText: {
-            fontWeight: 'bold',
-            color: theme.colors.text,
-            marginBottom: textMarginBottom || 0,
-            alignItems: 'center',
-            justifyContent: 'space-between',
-        },
-        subtitleText: {
-            color: theme.colors.text,
-            fontSize: 12,
-            fontWeight: '500',
-        },
-        tagText: {
-            color: theme.colors.text,
-            position: 'absolute',
-            fontSize: 12,
-            fontWeight: 'bold',
-            top: 11.2,
-            right: 10,
-        },
-    });
+          {sideTag !== undefined && sideTag !== null ? (
+            <Text style={styles.tagText}>{sideTag}</Text>
+          ) : null}
 
-    return (
-        <Animated.View
-            style={styles.container}
-            entering={FadeInDown.delay(index ? index * 100 : 0)}
-        >
-            <Pressable
-                android_ripple={styles.ripple}
-                style={styles.pressableContainer}
-                onPress={onClick || null}
-            >
-                <Suspense fallback={<ActivityIndicator />}>
-                    <Animated.Image
-                        style={
-                            style === 'side'
-                                ? imageStyles.side
-                                : imageStyles.fill
-                        }
-                        source={
-                            image || {
-                                uri:
-                                    uri ||
-                                    'https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png',
-                            }
-                        }
-                        sharedTransitionTag={animationTag || null}
-                    />
-                </Suspense>
-                <View style={styles.bodyContainer}>
-                    <View>
-                        {title ? (
-                            <Text style={styles.titleText}>{title}</Text>
-                        ) : null}
-                        {subtitle ? (
-                            <Text style={styles.subtitleText}>{subtitle}</Text>
-                        ) : null}
-                    </View>
-
-                    {sideTag !== undefined && sideTag !== null ? (
-                        <Text style={styles.tagText}>{sideTag}</Text>
-                    ) : null}
-
-                    {body ? body : null}
-                </View>
-            </Pressable>
-        </Animated.View>
-    );
+          {body ? body : null}
+        </View>
+      </Pressable>
+    </Animated.View>
+  );
 };
 
 export default ImageItemCard;
