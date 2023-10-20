e6b597100fe9a3ec059dabd94299bb838c01dab5
Dinushkasam <dinushkasam@gmail.com>
2023-10-20 23:35:06 +0530

    increase image upload limit

--- backend/routes/adoptionRoutes.js
+++ backend/routes/adoptionRoutes.js
@@ -32,7 +32,7 @@ router
   .route('/upload/:id')
   .post(
     protect,
-    multermiddleware.fields([{ name: 'images', maxCount: 1 }]),
+    multermiddleware.fields([{ name: 'images', maxCount: 10 }]),
     uploadImagesToAdoption
   );
 
