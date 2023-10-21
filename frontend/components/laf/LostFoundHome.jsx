a2c9b0e9ea9dfdd4e5b7e675818b1041409105bd
Navod <54998118+navodveduth@users.noreply.github.com>
2023-10-21 19:59:38 +0530

    laf post search function

--- frontend/components/laf/LostFoundHome.jsx
+++ frontend/components/laf/LostFoundHome.jsx
@@ -2,11 +2,12 @@ import React, { Suspense, useState, useEffect } from 'react';
 import { ActivityIndicator, ScrollView, View } from 'react-native';
 import getThemeContext from '../../context/ThemeContext';
 import { getAppContext } from '../../context/AppContext';
+import Toast from 'react-native-toast-message';
 
 import Search from '../common/Search';
 import PetsContainer from '../common/PetsContainer';
 
-import { getAllPosts } from '../../services/PostServices';
+import { getAllPosts, searchPosts } from '../../services/PostServices';
 
 const LostFoundHome = ({ navigation }) => {
   const { theme } = getThemeContext();
@@ -14,6 +15,8 @@ const LostFoundHome = ({ navigation }) => {
   const [lostPosts, setLostPosts] = useState([]);
   const [foundPosts, setFoundPosts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [searching, setSearching] = useState(false);
 
   const getPosts = async () => {
     try {
@@ -29,7 +32,14 @@ const LostFoundHome = ({ navigation }) => {
       );
       setFoundPosts(foundPosts);
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2:
+          error?.response?.data?.message ||
+          error.message ||
+          'Could not get posts',
+      });
     }
     setLoading(false);
   };
@@ -51,29 +61,74 @@ const LostFoundHome = ({ navigation }) => {
   const lostPetsPairs = groupIntoPairs(lostPosts.slice(0, 4));
   const foundPetsPairs = groupIntoPairs(foundPosts.slice(0, 4));
 
+  const handleSearch = async (text) => {
+    setSearchText(text);
+
+    if (text === '') {
+      await getPosts();
+      setSearching(false);
+      return;
+    }
+
+    try {
+      setSearching(true);
+      const response = await searchPosts(text, user.token);
+      const lostPosts = response.filter(
+        (post) => post.type === 'Lost' && post.status === 'open'
+      );
+      setLostPosts(lostPosts);
+
+      const foundPosts = response.filter(
+        (post) => post.type === 'Found' && post.status === 'open'
+      );
+      setFoundPosts(foundPosts);
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Error',
+        text2:
+          error?.response?.data?.message ||
+          error.message ||
+          'Could not get posts',
+      });
+    } finally {
+      setSearching(false);
+    }
+  };
+
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
-      <Search navigation={navigation} image={user.profilePic} profile={true} />
+      <Search
+        navigation={navigation}
+        image={user.profilePic}
+        profile={true}
+        text={searchText}
+        onChangeText={handleSearch}
+      />
       <Suspense fallback={<ActivityIndicator />}>
         <ScrollView>
-          <PetsContainer
-            header="Lost Pets"
-            btnText="See All"
-            pairs={lostPetsPairs}
-            component="FoundPost"
-            screen="Post"
-            fontSize={16}
-            loading={loading}
-          />
-          <PetsContainer
-            header="Found Pets"
-            btnText="See All"
-            pairs={foundPetsPairs}
-            component="FoundHome"
-            screen="Post"
-            fontSize={16}
-            loading={loading}
-          />
+          {lostPosts.length > 0 && (
+            <PetsContainer
+              header={searching ? 'Searching...' : 'Lost Pets'}
+              btnText="See All"
+              pairs={lostPetsPairs}
+              component="PostProfile"
+              screen="Post"
+              fontSize={16}
+              loading={loading}
+            />
+          )}
+          {foundPosts.length > 0 && (
+            <PetsContainer
+              header={searching ? 'Searching...' : 'Found Pets'}
+              btnText="See All"
+              pairs={foundPetsPairs}
+              component="FoundHome"
+              screen="Post"
+              fontSize={16}
+              loading={loading}
+            />
+          )}
         </ScrollView>
       </Suspense>
     </View>
